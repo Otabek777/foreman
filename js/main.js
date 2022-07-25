08323fe4f4734586a5eba8be308cae673b68e9df
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-07-25 07:19:01 +0500

    web page

---
+++ js/main.js
@@ -0,0 +1,30 @@
+if(document.querySelector(".switching")) {
+    $(".switching #btn1").click(function() {
+        $(this).addClass("active");
+        $(".switching #btn2").removeClass("active");
+    });
+    $(".switching #btn2").click(function() {
+        $(this).addClass("active");
+        $(".switching #btn1").removeClass("active");
+    });
+};
+
+if(document.querySelector(".btn_open")) {
+    $(".btn_open").click(function() {
+        $(this).toggleClass("active");
+    });
+}
+
+if(document.querySelector("#modal-card1")) {
+    $(".card__right .btn1").click(function() {
+        $("#modal-card1").addClass("active")
+    });
+    $(".card__right .btn2").click(function() {
+        $("#modal-card2").addClass("active")
+    });
+};
+if(document.querySelector(".cabinet_ads__item .options")) {
+    $(".cabinet_ads__item .options .btn").click(function() {
+        $(this).toggleClass("active")
+    });
+};
